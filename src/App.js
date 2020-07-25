0dbf88707ea09393810a307a3dcd2b5dfd924377
Martin Molinares Araujo <memolinares@uninorte.edu.co>
2020-07-25 18:24:55 -0500

    feat: hitting API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from '@emotion/styled';
+import axios from 'axios';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const [quote, setQuote] = useState({
+    quote: '',
+    author: ''
+  });
+  
+  const getQuote = async () => {
+    const response = await axios.get('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    console.log(response.data);
+    setQuote(response.data[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button
+        onClick={getQuote}
+      >
+        Get quote
+      </Button>
+      {quote.quote}
+    </Container>
   );
 }
 
